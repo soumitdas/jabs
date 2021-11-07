1b4c15da9c71f1eb3222d54a58fddf908e109aff
soumitdas <50671572+soumitdas@users.noreply.github.com>
2021-11-07 19:25:42 +0530

    Add loaders module

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,62 +2,28 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-const mongoose = require("mongoose");
 const express = require("express");
-const cors = require("cors");
-const { HttpError } = require("./utils/helper");
+const mongooseLoader = require("./loaders/mongoose");
+const expressAppLoader = require("./loaders/express");
 
-const app = express();
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(() => console.log("MongoDB Connected..."))
-  .catch((e) => console.error(e));
+async function startApplication() {
+  try {
+    const app = express();
 
-app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_BASE_URL,
-    optionsSuccessStatus: 200,
-  })
-);
-app.disable("x-powered-by");
+    await mongooseLoader();
+    await expressAppLoader(app);
 
-const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/user");
-const categoryRoutes = require("./routes/category");
-const productRoutes = require("./routes/product");
-const orderRoutes = require("./routes/order");
-
-app.use("/auth", authRoutes);
-app.use("/users", userRoutes);
-app.use("/categories", categoryRoutes);
-app.use("/products", productRoutes);
-app.use("/orders", orderRoutes);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new HttpError(404, "Not Found");
-  next(err);
-});
-
-// Error Handelling
-app.use((err, req, res, next) => {
-  if (err) {
-    if (!err.statusCode) {
-      err.statusCode = err.name === "UnauthorizedError" ? 401 : 400;
-    }
-    return res.status(err.statusCode).json({
-      status: "ERROR",
-      data: {},
-      message: err.message,
-    });
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => console.log(`Server started at PORT ${PORT}...`));
+  } catch (error) {
+    console.error("Server starting failed");
+    console.error(error);
+    process.exit();
   }
-  next();
-});
+}
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server started at PORT ${PORT}...`));
+startApplication();
+
+
+
+
